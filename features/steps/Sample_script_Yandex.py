61950b993493b9e90cb0b83c8be43bbf3f0a4102
King Kong <gurovvic@gmail.com>
2019-10-06 21:29:35 -0400

    modified and deleted and created nine files

---
+++ features/steps/Sample_script_Yandex.py
@@ -0,0 +1,30 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# init driver
+driver = webdriver.Chrome()
+
+# open the url
+driver.get('https://yandex.ru/')
+
+
+search = driver.find_element(By.NAME, 'text')
+search.clear()
+search.send_keys('Management')
+
+
+# wait for 4 sec
+sleep(4)
+
+
+# click search
+driver.find_element(By.CLASS_NAME, 'search2__button').click()
+
+
+# verify
+assert 'Management' in driver.find_element(By.XPATH, "//div[contains(@class,'text-container typo typo_text_m typo_line_m')]").text
+assert 'Management' in driver.find_element(By.XPATH, "//div[@class='text-container typo typo_text_m typo_line_m']").text
+
+driver.quit()
+
--- features/tests/my_product_search_2.feature
+++
@@ -1,8 +0,0 @@
-# Created by Gurov Viacheslav at 05.10.2019
-Feature: Google search test
-
-  Scenario: Test google search
-    Given I am on google search page
-    When I type in text to search
-    And I hit the search button
-    Then I should be on the search results page
