1acf0e6477c4cdeebcddd868ff0e4bbf6f681afd
King Kong <gurovvic@gmail.com>
2019-10-11 22:18:45 -0400

    modified environment.py and  main_page_steps and orders.feature and a new file created  sign_in_page_steps.py

--- features/steps/main_page_steps.py
+++ features/steps/main_page_steps.py
@@ -1,8 +1,8 @@
 from behave import given, when, then
 from selenium.webdriver.common.by import By
 
-ORDERS_LINK = (By.CSS_SELECTOR, "a#nav-orders span.nav-line-2")
-
+ORDERS_LINK = (By.CSS_SELECTOR, "a#nav-orders span.nav-line-2") #type of data tuple-can not be changed
+#EMAIL_FIELD = (By.CSS_SELECTOR, "input[type='email']")
 
 @given('Open Amazon page')
 def open_amazon(context):
@@ -11,10 +11,11 @@ def open_amazon(context):
 
 @when('Click Amazon Orders Link')
 def click_orders_link(context):
-    context.driver.find_element(*ORDERS_LINK).click()
+    context.driver.find_element(*ORDERS_LINK).click()  #*-tells that there are two elements
 
-
+"""
 @then("Verify Sign In page is opened")
 def verify_signin_opened(context):
-    context.driver.find_element(By.CSS_SELECTOR, "input[type='email']")
-    assert 'https:///wwww.amazon.com/ap/signin' in context.driver.current_url
+    context.driver.find_element(*EMAIL_FIELD) #*-tells that there are two elements
+    assert 'https://www.amazon.com/ap/signin' in context.driver.current_url
+"""
--- features/tests/orders.feature
+++ features/tests/orders.feature
@@ -2,7 +2,7 @@
 Feature: Tests for Orders functionality
   # Enter feature description here
 
-  Scenario: Logged out user sees Sign in page
+  Scenario: Logged out user sees Sign in page when click
     Given Open Amazon page
     When Click Amazon Orders Link
     Then Verify Sign In page is opened
