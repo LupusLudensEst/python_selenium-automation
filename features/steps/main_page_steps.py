b7b324cfcdf9801714c0fed5ef85a193ce12bd20
King Kong <gurovvic@gmail.com>
2019-10-28 00:06:39 -0400

    maimai

--- features/steps/main_page_steps.py
+++ features/steps/main_page_steps.py
@@ -1,21 +1,42 @@
-from behave import given, when, then
+from behave import when, given, then
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
 
 ORDERS_LINK = (By.CSS_SELECTOR, "a#nav-orders span.nav-line-2") #type of data tuple-can not be changed
-#EMAIL_FIELD = (By.CSS_SELECTOR, "input[type='email']")
+EMAIL_FIELD = (By.CSS_SELECTOR, "input[type='email']")
+CART = (By.ID, 'nav-cart')
+CART_EMPTY = (By.CSS_SELECTOR, "h1.sc-empty-cart-header")
 
 @given('Open Amazon page')
 def open_amazon(context):
     context.driver.get('https://www.amazon.com/')
 
+@given('Open Amazon Bestsellers')
+def open_amazon_bestsellers(context):
+    context.driver.get('https://www.amazon.com/Best-Sellers/zgbs/ref=zg_bs_tab')
 
 @when('Click Amazon Orders Link')
 def click_orders_link(context):
     context.driver.find_element(*ORDERS_LINK).click()  #*-tells that there are two elements
 
-"""
-@then("Verify Sign In page is opened")
+@when('Click on cart icon')
+def click_cart_icon(context):
+    cart_icon = context.driver.find_element(*CART)
+    print(cart_icon)
+    context.driver.refresh()
+    cart_icon = context.driver.find_element(*CART)
+    print(cart_icon)
+    cart_icon.click()
+
+@then("Verify 'Your Shopping Cart is empty.' text present")
+def verify_cart_empty(context):
+    context.driver.find_element(*CART_EMPTY )
+    assert 'Your Shopping Cart is empty.' in context.driver.find_element(*CART_EMPTY).text
+
+
+@then("Verify Sign In page is open")
 def verify_signin_opened(context):
     context.driver.find_element(*EMAIL_FIELD) #*-tells that there are two elements
     assert 'https://www.amazon.com/ap/signin' in context.driver.current_url
-"""
--- features/tests/orders.feature
+++ features/tests/orders.feature
@@ -1,8 +1,13 @@
 # Created by Gurov Viacheslav at 07.10.2019
 Feature: Tests for Orders functionality
-  # Enter feature description here
+  #hw6.1_following_lana
 
   Scenario: Logged out user sees Sign in page when click on it
     Given Open Amazon page
     When Click Amazon Orders Link
-    Then Verify Sign In page is opened
+    Then Verify Sign In page is open
+
+   Scenario: 'Your Shopping Cart is empty' shown if no product added
+    Given Open Amazon page
+    When Click on cart icon
+    Then Verify 'Your Shopping Cart is empty.' text present
