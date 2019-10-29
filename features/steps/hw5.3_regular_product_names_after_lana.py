83df8c8f392b3688574083e162dfa2cfe37507f2
King Kong <gurovvic@gmail.com>
2019-10-28 20:22:41 -0400

    hw5.3 updated 28.10.2019 hw5.3_regular_product_names_after_lana.py

--- features/steps/hw5.3_regular_product_names_after_lana.py
+++ features/steps/hw5.3_regular_product_names_after_lana.py
@@ -4,8 +4,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
-PRODUCTS = (By.XPATH, "//*[@id='wfm-pmd_deals_section']/div[6]//li") #All products as search field
-PRODUCT_NAME = (By.CSS_SELECTOR, 'span.wfm-sales-item-card__product-name') #All strings on the page consisting expected word
+PRODUCTS = (By.XPATH, "//*[@id='wfm-pmd_deals_section']/div[6]//li") #12 or 15 goods below the agreed line, all products as search field
+PRODUCT_DESCRIPTION = (By.CSS_SELECTOR, 'span.wfm-sales-item-card__product-name') #All strings on the page consisting description of the goods
 
 @given("Open Amazon {productid} page as Lana did")
 def open_wholefoods_deals(context, productid):
@@ -18,6 +18,6 @@ def verify_regular_price(context, expected_word):
     for product_element in product_elements:
         assert expected_word in product_element.text, f"Expected, '{expected_word}', to be in: {product_element.text}"
 
-        product_name = product_element.find_element(*PRODUCT_NAME).text
-        print('\n', product_name, ';')
-        assert '' != product_name, f'Expected non-empty product name'
+        product_description = product_element.find_element(*PRODUCT_DESCRIPTION).text
+        print('\n', product_description, ';')
+        assert '' != product_description, f'Expected non-empty product name'
