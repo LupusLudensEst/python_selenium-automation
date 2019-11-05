37c55f61d1ac9df5f6ab75bf6944b1bb2224fdac
King Kong <gurovvic@gmail.com>
2019-11-04 22:48:03 -0500

    hw6.2 upgrade dt 04.11.2019

--- features/steps/hw6.2_bestsellers_page_steps.py
+++ features/steps/hw6.2_bestsellers_page_steps.py
@@ -1,12 +1,14 @@
 from behave import when, then
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
+#This method allows bypass "Stale element exception"
 #Locators declared as an variables
 BEST_SELLERS_PAGE = (By.XPATH, "//a[contains(@href, 'bestsellers')]") #(By.XPATH, "//a[@class='nav-a  ']")
-TOP_LINKS = (By.CSS_SELECTOR, '#zg_tabs a')
+TOP_LINKS = (By.XPATH, "//a[contains(@href, 'zg_bsms_tab')]") #(By.CSS_SELECTOR, "li.zg_selected") #(By.CSS_SELECTOR, '#zg_tabs a')
 HEADER = (By.CSS_SELECTOR, '#zg_banner_text_wrapper')
+
+#Given 'Open Amazon page' works with the step in another file-it finds it by default
 
 @when('Open Best Sellers page')
 def click_on_best_sellers_page(context):
