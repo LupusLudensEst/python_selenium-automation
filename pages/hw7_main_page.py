d91e599a8991a8aa3343d45697e1866487f6451a
King Kong <gurovvic@gmail.com>
2019-11-13 22:12:14 -0500

    hw8 modified hw7_main_page.py

--- pages/hw7_main_page.py
+++ pages/hw7_main_page.py
@@ -4,9 +4,13 @@ from pages.hw7_base_page import Page
 class MainPage(Page):
     SEARCH_INPUT = (By.ID, 'twotabsearchtextbox')
     SEARCH_ICON = (By.CSS_SELECTOR, "input.nav-input[type='submit']")
+    HAM_MENU = (By.ID, 'nav-hamburger-menu')
 
     def search_for_keyword(self, text):
         self.input_text(text, *self.SEARCH_INPUT)
         self.click(*self.SEARCH_ICON)
+
+    def click_menu(self):
+        self.click(*self.HAM_MENU)
 
 
