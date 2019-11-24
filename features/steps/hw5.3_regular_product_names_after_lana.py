dee26e574ebd3adc371fd1f0e3bb8b6b73376a6f
King Kong <gurovvic@gmail.com>
2019-11-23 23:09:11 -0500

    honing the project for Allure dt 23 Nov 2019

--- features/steps/hw5.3_regular_product_names_after_lana.py
+++ features/steps/hw5.3_regular_product_names_after_lana.py
@@ -7,9 +7,9 @@ from time import sleep
 PRODUCTS = (By.XPATH, "//*[@id='wfm-pmd_deals_section']/div[6]//li") #12 or 15 goods below the agreed line, all products as search field
 PRODUCT_DESCRIPTION = (By.CSS_SELECTOR, 'span.wfm-sales-item-card__product-name') #All strings on the page consisting description of the goods
 
-@given("Open Amazon {productid} page as Lana did")
-def open_wholefoods_deals(context, productid):
-    context.driver.get((f'https://www.amazon.com/{productid}/'))
+@given("Open Amazon {product_id} page as Lana did")
+def open_wholefoods_deals(context, product_id):
+    context.driver.get((f'https://www.amazon.com/{product_id}/'))
 
 @then("Verify every product on the page has a text {expected_word} and a product name as Lana did")
 def verify_regular_price(context, expected_word):
@@ -17,6 +17,7 @@ def verify_regular_price(context, expected_word):
 
     for product_element in product_elements:
         assert expected_word in product_element.text, f"Expected, '{expected_word}', to be in: {product_element.text}"
+        print('Product element: ',  product_element.text, '===')
 
         product_description = product_element.find_element(*PRODUCT_DESCRIPTION).text
         print('\n', product_description, ';')
