2e350382689d84276253e8cf12515c9e16430704
LupusLudensEst <gurovvic@gmail.com>
2019-11-30 11:37:28 -0500

    upgraded  Lingocard_text_on_main_page.py dt 30 Nov 2019

--- features/steps/Lingocard_text_on_main_page.py
+++ features/steps/Lingocard_text_on_main_page.py
@@ -10,39 +10,40 @@ from time import sleep
 #TEXT_HERE = (By.XPATH, "//h1[@class='header-title flex-container content-center text-center lang']")
 #ELEVEN_ITEMS_HERE = (By.CSS_SELECTOR, "ul.white")
 
-# init driver
-driver = webdriver.Chrome()
+# # init driver
+# driver = webdriver.Chrome()
+#
+# # open the url
+# driver.get('https://lingocard.com/')
+# driver.maximize_window()
 
-# open the url
-driver.get('https://lingocard.com/')
-driver.maximize_window()
-
-# @given('Open Lingocard page')
-# def open_lingocard(context):
-#     context.driver.get('https://lingocard.com/')
-#     sleep(3)
+@given('Open Lingocard page')
+def open_lingocard(context):
+    context.driver.get('https://lingocard.com/')
+    sleep(3)
 
 # Verify if "International Educational Platform" is on page
-assert 'International Educational Platform' in driver.find_element(By.XPATH, "//h1[@class='header-title flex-container content-center text-center lang']").text
-print('The text: ', driver.find_element(By.XPATH, "//h1[@class='header-title flex-container content-center text-center lang']").text, '.')
-sleep(3)
+# assert 'International Educational Platform' in driver.find_element(By.XPATH, "//h1[@class='header-title flex-container content-center text-center lang']").text
+# print('The text: ', driver.find_element(By.XPATH, "//h1[@class='header-title flex-container content-center text-center lang']").text, '.')
+# sleep(3)
 
-# @then('Verify if "International Educational Platform" is on page')
-# def verify_text_here(context):
-#     assert 'International Educational Platform' in driver.find_element(By.XPATH, "//h1[@class='header-title flex-container content-center text-center lang']").text
-#     print('The text: ', driver.find_element(By.XPATH, "//h1[@class='header-title flex-container content-center text-center lang']").text, '.')
-#     sleep(3)
+@then('Verify if "International Educational Platform" is on page')
+def verify_text_here(context):
+    assert 'International Educational Platform' in context.driver.find_element(By.XPATH, "//h1[@class='header-title flex-container content-center text-center lang']").text
+    print('The text: ', context.driver.find_element(By.XPATH, "//h1[@class='header-title flex-container content-center text-center lang']").text, '.')
+    sleep(3)
 
 # # Verify if there are 11 boxes in the hamburger menu
-driver.find_element(By.XPATH, "//div[@class='menu-link']").click()
-sleep(3)
-print('There are: ', len(driver.find_elements(By.CSS_SELECTOR, "ul.white")), 'items total.')
-sleep(3)
+# driver.find_element(By.XPATH, "//div[@class='menu-link']").click()
+# sleep(3)
+# print('There are: ', len(driver.find_elements(By.CSS_SELECTOR, "ul.white")), 'items total.')
+# sleep(3)
 
-# @then('Verify if there are 11 boxes in the hamburger menu')
-# def verify_items_here(context):
-#     driver.find_element(By.XPATH, "//div[@class='menu-link']").click()
-#     sleep(3)
-#     print('There are: ', len(driver.find_elements(By.CSS_SELECTOR, "ul.white")), 'items total.')
-
-driver.quit()
+@then('Verify if there are 11 boxes in the hamburger menu')
+def verify_items_here(context):
+    context.driver.find_element(By.XPATH, "//div[@class='menu-link']").click()
+    sleep(1)
+    menu_items = context.driver.find_elements(By.CSS_SELECTOR, "#menu ul a")
+    assert 11 == len(menu_items), f'Expected 11, but got {len(menu_items)} items total.'
+    print('There are: ', len(context.driver.find_elements(By.CSS_SELECTOR, "#menu ul a")), 'items total.')
+# driver.quit()
