b723d936fcc297f91d3c600be4d3f5db32efc70f
LupusLudensEst <gurovvic@gmail.com>
2019-12-10 00:35:24 -0500

    created Bel_sample_dt_10_Dec_2019.py

--- Bel_sample_dt_10_Dec_2019.py
+++ Bel_sample_dt_10_Dec_2019.py
@@ -8,7 +8,7 @@ driver = webdriver.Chrome()
 
 # open the url
 driver.get('https://www.discountmugs.com/')
-sleep(4)
+sleep(2)
 
 assert 'Empowering you to share your message and create a lasting impression since 1995' in driver.find_element(By.XPATH, "//div[@class='home-line-text']").text
 print(driver.find_element(By.XPATH, "//div[@class='home-line-text']").text)
