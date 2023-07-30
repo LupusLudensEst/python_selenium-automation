c2cb1962f6a55067f64d6bd68204e97d29adc89c
Vic Gurov <gurovvic@gmail.com>
2023-07-29 19:55:45 -0500

    dt 29 jul 2023

--- TDD/alert_dt_09_june_2020.py
+++ TDD/alert_dt_09_june_2020.py
@@ -1,14 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-# driver = webdriver.Chrome(executable_path = "C:\Webdrivers\chromedriver.exe")
 driver = webdriver.Chrome()
-driver.get("https://testautomationpractice.blogspot.com/")
 driver.maximize_window()
+
+driver.get("https://testautomationpractice.blogspot.com/")
+
 driver.implicitly_wait(5)
 
 # Locators
-CLCK_ME_BTN = (By.XPATH, "//button[@onclick='myFunction()']")
+CLCK_ME_BTN = (By.XPATH, "//button[@onclick='myFunctionAlert()']")
 
 driver.find_element(*CLCK_ME_BTN).click()
 
@@ -16,4 +17,4 @@ driver.find_element(*CLCK_ME_BTN).click()
 driver.switch_to.alert.dismiss() # closes alert window using cancel button
 
 # Driver quit
-driver.quit()
+# driver.quit()
--- TDD/boats_group_dt_22_feb_2020.py
+++ TDD/boats_group_dt_22_feb_2020.py
@@ -1,16 +1,21 @@
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
 driver.maximize_window()
-driver.implicitly_wait(20)
+
 
 # locators
 CRS_BTN = (By.XPATH, "//li[@class='menu-item menu-item-type-post_type menu-item-object-page menu-item-3954']")
-QA_ENG_MIA = (By.XPATH, "//a[@ns-qa='QA Automation Engineer']")
-QA_TXT_HR = (By.ID, "gnewtonJobDescriptionText")
+IFRM_LCTR = (By.ID, "gnewtonIframe")
+SNR_SFTWR_DVLPR_ENG_MIA = (By.XPATH, "//a[@ns-qa='Senior Software Developer']")
+SNR_SFTWR_DVLPR_ENG_MIA_TXT_HR = (By.ID, "gnewtonJobDescriptionText")
+
+# Explicit wait
+wait = WebDriverWait(driver, 15)
 
 # open the url
 driver.get( 'https://www.boatsgroup.com/' )
@@ -19,15 +24,21 @@ driver.get( 'https://www.boatsgroup.com/' )
 driver.find_element( *CRS_BTN ).click()
 
 # open iframe
-driver.switch_to.frame(driver.find_element_by_id("gnewtonIframe"))
+# driver.switch_to.frame(driver.find_element( *IFRM_LCTR ))
+wait.until(EC.frame_to_be_available_and_switch_to_it("gnewtonIframe"))
 
 # click on QA Automation Engineer in Miami button
-driver.find_element( *QA_ENG_MIA ).click()
+driver.find_element( *SNR_SFTWR_DVLPR_ENG_MIA ).click()
 
-# verify page has a text 'Invalid user name or password.'
-actual_text = driver.find_element( *QA_TXT_HR ).text
-assert 'As QA Automation Engineer you will develop automated test solutions' in actual_text
-print(f'Text is here:\n {actual_text} \n!')
+# verify page has a text 'Senior Software Developer - Hybrid (Miami)'
+sleep(8)
+expected_text = 'Senior Software Developer - Hybrid (Miami)'
+actual_text = driver.find_element( *SNR_SFTWR_DVLPR_ENG_MIA_TXT_HR).text
+assert expected_text in actual_text
+print(f'Text is here:\n {actual_text[:len(expected_text)]}')
 
-# driver quit
-driver.quit()
+# # Sleep to see what we have
+sleep(2)
+#
+# # Driver quit
+# driver.quit()
